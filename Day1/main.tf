65b2eb06b3634cec0f17dfa0328f11fdefec9fe8
EC2 Default User <ec2-user@ip-172-31-15-162.ap-south-2.compute.internal>
2026-06-29 09:57:38 +0000

    added new folders

---
+++ Day1/main.tf
@@ -0,0 +1,9 @@
+#configure cloud provider and region
+provider "aws" {
+        region = "ap-south-2"
+}
+# configure ami details and instance type
+resource "aws_instance" "example" {
+      ami = "ami-01f3f4b95d125a9af"    # setting ami details
+      instance_type = "t3.micro"
+}
