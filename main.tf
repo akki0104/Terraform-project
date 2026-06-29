4cbc98edabe02cbf9f220cb79f90e61684f742f4
EC2 Default User <ec2-user@ip-172-31-15-162.ap-south-2.compute.internal>
2026-06-29 12:07:39 +0000

    added

--- main.tf
+++ main.tf
@@ -1,12 +1,12 @@
 #configure cloud provider and region
 provider "aws" {
         region = "ap-south-2"
-       # alias = "ap-south-2"
+        alias = "ap-south-2"
 }
 
 provider "aws" {
         region = "ap-south-1"
-        # alias = "ap-south-1"
+         alias = "ap-south-1"
 }
 # configure ami details and instance type
 resource "aws_instance" "example" {
